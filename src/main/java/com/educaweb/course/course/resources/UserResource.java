f4cdc1fd4e3ade4fd6e6c1c5d66dc0a27acdcac4
jonathansousa87 <jonathandrumbass@gmail.com>
2022-03-26 16:49:40 -0300

    User update

--- src/main/java/com/educaweb/course/course/resources/UserResource.java
+++ src/main/java/com/educaweb/course/course/resources/UserResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.persistence.PostUpdate;
 import java.net.URI;
 import java.util.List;
 
@@ -40,6 +41,11 @@ public class UserResource {
     public ResponseEntity<Void> delete(@PathVariable Long id){
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id,@RequestBody User obj){
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
